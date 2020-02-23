f16bed50ccfb48e9b37376f3d96fb0a3b4a3e9f4
JefersonUrbieta <jeferson.urbieta@gmail.com>
2020-02-23 21:49:35 +0000

    Deploying to gh-pages from  - 9e7592ebc6afeb4ebf938c7cb45086fc6d0a354b 🚀

---
+++ precache-manifest.128578eb65bc6d88a306a4911241cd54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a232b6ea336bbf5365c",
+    "url": "/css/app.6e778e8b.css"
+  },
+  {
+    "revision": "99d4be67430cf20ffefd",
+    "url": "/css/chunk-vendors.26578e2b.css"
+  },
+  {
+    "revision": "f26ddb82757de95ab73920a81297595b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a232b6ea336bbf5365c",
+    "url": "/js/app.fdf43b4a.js"
+  },
+  {
+    "revision": "99d4be67430cf20ffefd",
+    "url": "/js/chunk-vendors.13034472.js"
+  },
+  {
+    "revision": "4b28740ab3053968e1696cfcd4e07522",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
